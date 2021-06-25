7b4c6352f476659865c335ee9f40761245f5c799
Leonardo Leão <leonardoleao486@gmail.com>
2021-06-25 08:50:08 -0300

    treatment data script 2.0

---
+++ BasicFunctions.py
@@ -0,0 +1,78 @@
+"""
+    @Author: Leonardo Rossi Leao / Rodrigo de Oliveira Neto
+    @Create: June 26, 2021
+    @Title: Dat file monitor
+"""
+
+import os
+from datetime import datetime, timedelta
+
+class BasicFunctions:
+    
+    @staticmethod
+    def getDatetime():
+        now = datetime.now()
+        return now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    @staticmethod
+    def recordAction(text):
+        monitor = open("monitor.txt", "a")
+        datetime = "[" + BasicFunctions.getDatetime() + "] "
+        print(datetime + text + "\n")
+        monitor.write(datetime + text + "\n")
+        monitor.close()
+        
+
+class DatFunctions:
+    
+    @staticmethod
+    def processFile(data, channel):
+        seismicData = ""
+        aSeismicData = []
+        # Obtem as informacoes sobre a captacao de dados
+        data = data.replace("$", "").split("\n")
+        sampleRate = float(DatFunctions.getValue(data[3]))
+        initialDate = DatFunctions.processDatetime(data[4])
+        date = DatFunctions.processDatetime(data[4])
+        bitWeight = float(DatFunctions.getValue(data[5]))
+        # Processa os valores sismicos recebidos 
+        for counts in data[9:len(data)-1]:
+            speed = float(counts) * bitWeight / 800
+            aSeismicData.append(speed)
+            seismicData += str(datetime.timestamp(date)) + ": %.30f" % speed + "\n"
+            date = date + timedelta(milliseconds = 1000/sampleRate)
+        # Armazena os valores sismicos processados
+        DatFunctions.recordData(initialDate, seismicData, channel)           
+        
+    @staticmethod
+    def getValue(data):
+        option, value = tuple(data.replace(" ", "").split("="))
+        return value
+    
+    @staticmethod
+    def processDatetime(date):
+        date = DatFunctions.getValue(date)
+        year, day, hour, minutes, seconds, ms = tuple(date.replace(".", ":").split(":"))
+        dt = datetime(int(year), 1, 1) + timedelta(int(day) - 1)
+        return datetime(dt.year, dt.month, dt.day, int(hour), int(minutes), int(seconds), int(ms)*100)
+    
+    @staticmethod
+    def howToRecord(date):
+        year = date.year
+        day = (date - datetime(year, 1, 1)).days + 1
+        hour = date.hour
+        return (year, day, str(hour) + ".txt")
+    
+    @staticmethod
+    def recordData(date, seismic, channel):
+        pathIn = "C:/Users/ASUS/Desktop/SeismicData/"
+        path = (pathIn + "%s/%s/" + channel + "/%s") % DatFunctions.howToRecord(date)
+        os.makedirs(os.path.dirname(path), exist_ok = True)
+        with open(path, "a") as file:
+            file.write(seismic)
+        file.close()
+    
+    
+    
+    
+    
---
+++ DatMonitor.py
@@ -0,0 +1,81 @@
+"""
+    @Author: Leonardo Rossi Leao / Rodrigo de Oliveira Neto
+    @Create: June 26, 2021
+    @Title: Dat file monitor
+"""
+
+# Libraries
+import os, time, threading
+from datetime import datetime
+from BasicFunctions import BasicFunctions
+from BasicFunctions import DatFunctions
+
+class DatMonitor(threading.Thread):
+    
+    def __init__(self):
+        super(DatMonitor, self).__init__()
+        self.kill = threading.Event()
+        self.pathIn = "/home/reftek/seismicstation/"
+        # self.pathIn = "C:/Users/ASUS/Desktop/ArchiverScripts/"
+        
+    def searchFiles(self):
+        arquivos = set(os.listdir(self.pathIn))
+        return arquivos
+    
+    def run(self):
+        while not self.kill.is_set():
+            content = self.searchFiles()
+            for filename in content:
+                if ".atr" in filename:
+                    try:
+                        BasicFunctions.recordAction("DAT: file founded")
+                        file = open(self.pathIn + filename, "r")
+                        data = file.read()
+                        file.close()
+                        DatFunctions.processFile(data, filename[-5])
+                        os.remove(self.pathIn + filename)
+                        BasicFunctions.recordAction("DAT: channel %s | concluded"  % filename[-5])
+                    except Exception as e:
+                        print(e.message, e.args)
+            time.sleep(0.5) 
+            
+class RawMonitor(threading.Thread):
+    
+    def __init__(self):
+        super(RawMonitor, self).__init__()
+        self.kill = threading.Event()
+        self.pathIn = "/home/reftek/bin/archive/"
+        # self.pathIn = "C:/Users/ASUS/Desktop/archive/"
+        self.pathCvt = self.pathIn + "RT_ASC"
+        
+    def searchFiles(self):
+        today = DatFunctions.howToRecord(datetime.now())
+        path = "%s%d%d/B67D/1/" % (self.pathIn, today[0], today[1])
+        files = set(os.listdir(path))
+        return (path, files)
+    
+    def convertion(self, path, newFile):
+        for file in newFile:
+            pathIn = path + file
+            if "_00000000" not in pathIn:
+                os.system(self.pathCvt + " -Ln " + pathIn)
+                os.remove(pathIn)
+                BasicFunctions.recordAction("RAW: file converted to DAT")
+                
+    def run(self):
+        BasicFunctions.recordAction("RAW: start raw monitor")
+        path, content = self.searchFiles()
+        while not self.kill.is_set():
+            path, newContent = self.searchFiles()
+            newFile = newContent.difference(content)
+            if newFile:
+                BasicFunctions.recordAction("RAW: file founded")
+                self.convertion(path, newFile)
+            content = newContent
+            time.sleep(0.5)
+            
+if __name__ == "__main__":
+    threadRM = RawMonitor()
+    threadRM.start()
+    threadDM = DatMonitor()
+    threadDM.start()
